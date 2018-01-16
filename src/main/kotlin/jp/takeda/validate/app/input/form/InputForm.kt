fab408ce44691905562eeeb3b178178ab250438f
takedasan <takedasan593@gmail.com>
2018-01-16 23:06:26 +0900

    Addd message resource

--- src/main/kotlin/jp/takeda/validate/app/input/form/InputForm.kt
+++ src/main/kotlin/jp/takeda/validate/app/input/form/InputForm.kt
@@ -6,6 +6,8 @@ import javax.validation.Valid
 class InputForm {
     @NotBlank
     var name: String = ""
+    @NotBlank(message="{E0001}")
+    var age: String = ""
     @Valid
     var detail: DetailInputForm = DetailInputForm()
 }
